d0852a936c2df088fd8cbf4967308ec091fbcc55
patel-udit-patel <udit0428t@gmail.com>
2025-03-06 13:49:20 +0530

    [Udit Patel] Add .To display Hello from BridgeLabz with a user inputted name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -12,7 +13,7 @@ export class AppComponent {
   title = 'Bridgelabz';
   logo = "1.jpg.jpg";
   bridgelabzURL="https://www.bridgelabz.com"
-
+  username:string='Your name';
   openBridgeLabz(){
     window.open(this.bridgelabzURL,'_blank')
   }
